97c81ace725e9850188c2c01a6db546915e5c2ef
aarnav <aarnavbos@gmail.com>
2022-04-02 13:35:25 +0200

    run isort

--- python/analyze.py
+++ python/analyze.py
@@ -15,10 +15,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
-from libs import libs
 import json
+
 import parserkiosk
 
+from libs import libs
 
 reports = {}
 dump = {}
